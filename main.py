1b5add5c039c341a0a66f218cced74a827b6cc03
ai-mayurpatil <ai.mayurpatil@gmail.com>
2024-11-14 15:55:54 +0530

    path operation done

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#request Get method url: "/"
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "welcom to my api"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This  is your posts"}
+
