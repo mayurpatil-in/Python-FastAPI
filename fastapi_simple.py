2ede235041c3cfae567d4ec104991deeada4dd73
ai-mayurpatil <ai.mayurpatil@gmail.com>
2025-04-11 09:54:35 +0530

    get method done

--- fastapi_simple.py
+++ fastapi_simple.py
@@ -3,5 +3,13 @@ import uvicorn
 
 app = FastAPI(title="Simple fastapi tutorial")
 
+@app.get("/") #path
+def hello_world():
+    return {"message": "hello world"}
+
+@app.get("/hello/{name}") #path
+def hello_name(name: str):
+    return{"message": f"hello {name}"}
+
 if __name__ == "__main__":
     uvicorn.run("fastapi_simple:app" , host="0.0.0.0", port=8000, reload=True)
