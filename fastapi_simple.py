1811f1c76f0742b9718efff661cba106aa21b083
ai-mayurpatil <ai.mayurpatil@gmail.com>
2025-04-11 11:17:38 +0530

    post  done

--- fastapi_simple.py
+++ fastapi_simple.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI(title="Simple fastapi tutorial")
 
@@ -10,6 +11,27 @@ def hello_world():
 @app.get("/hello/{name}") #path
 def hello_name(name: str):
     return{"message": f"hello {name}"}
+
+class Todo(BaseModel):
+    title:str
+    completed:bool = False
+
+todos = []
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "todo created", "todo":todo}
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int):
+    if todo_id < 0 or todo_id >= len(todos):
+        return {"message": "todo not found"}
+    return todos[todo_id]
 
 if __name__ == "__main__":
     uvicorn.run("fastapi_simple:app" , host="0.0.0.0", port=8000, reload=True)
