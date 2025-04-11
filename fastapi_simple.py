e931bc3bd9731055fdcb1a60329ac628abf6365e
ai-mayurpatil <ai.mayurpatil@gmail.com>
2025-04-11 11:24:29 +0530

    put done

--- fastapi_simple.py
+++ fastapi_simple.py
@@ -33,5 +33,12 @@ def get_todo(todo_id:int):
         return {"message": "todo not found"}
     return todos[todo_id]
 
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo: Todo):
+    if todo_id < 0 or todo_id >= len(todos):
+        return {"message": "todo not found"}
+    todos[todo_id] = todo
+    return {"message": "todo updated", "todo":todo}
+
 if __name__ == "__main__":
     uvicorn.run("fastapi_simple:app" , host="0.0.0.0", port=8000, reload=True)
